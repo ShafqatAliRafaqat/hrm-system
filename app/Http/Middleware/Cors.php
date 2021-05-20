722afb4643751f36c69b6923f255aebe0df4ef7e
ShafqatAliRafaqat <Shafqat.Ali.786@outlook.com>
2021-05-20 21:31:41 +0500

    migrations

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,8 @@ class Cors
     {
         // return $next($request);
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*');
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
